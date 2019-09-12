86af694fa4abfbd92d9e772b0dbe63e0defb6947
syed <syedmohi04@gmail.com>
2019-09-12 15:22:53 +0530

    login view done

--- index.js
+++ index.js
@@ -2,23 +2,41 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const path = require('path');
+const uuidv1 = require('uuid/v1');
 const routes = require('./routes/routes');
+var session = require('express-session')
+var passport = require('passport');
+
 const PORT = process.env.PORT || 8000;
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public", "css")));
 
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,"public","css")));
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+    genid: function (req) {
+        return uuidv1() // use UUIDs for session IDs
+    },
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+
+    //   cookie: { secure: true } https true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get("/", routes.index);
+app.get("/register", routes.register);
+app.get("/login", routes.login);
+app.get("/dashboard",routes.dashboard);
+app.post("/register", routes.postRegister);
+app.post("/login", routes.postLogin);
 
 
 
-app.get("/",routes.index);
-app.get("/register",routes.register);
-app.get("/login",routes.login);
-app.post("/register",routes.postRegister);
 
 
-
-
-
-app.listen(PORT,()=>console.log('listening to port 8000'));
+app.listen(PORT, () => console.log('listening to port 8000'));
--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,17 @@ const Login = require('../db/db.config');
 const Joi = require('@hapi/joi');
 const bcrypt = require('bcrypt');
 
+
 const Schema = Joi.object({
     username: Joi.string().min(3).max(30).required(),
     email: Joi.string().required(),
     password: Joi.string().required()
 
+});
+
+const loginSchema = Joi.object({
+    email: Joi.string().required(),
+    password:Joi.string().required()
 });
 
 exports.index = function (req, res) {
@@ -17,7 +23,10 @@ exports.register = function (req, res) {
     res.render("register", { title: "Registration", error: undefined, value: undefined, err: undefined });
 }
 exports.login = function (req, res) {
-    res.render("login", { title: "Login" });
+    res.render("login", { title: "Login",error:undefined,msg:""});
+}
+exports.dashboard = function(req,res){
+    res.render('dashboard',{name:""})
 }
 
 exports.postRegister = function (req, res) {
@@ -56,5 +65,41 @@ exports.postRegister = function (req, res) {
     });
 
 
+}
+
+exports.postLogin = function (req,res) {
+    
+    const loginPost = {
+        email:req.body.email,
+        password:req.body.password
+    };
+    const { email,password } = loginPost;
+    loginSchema.validate({email:email,password:password},(err,value)=>{
+        if(err){
+            res.render("login",{title:"Login",error:err,msg:""})
+        } else {
+            Login.findOne({email:email},function(err,data){
+                if(err){
+                    console.log(err);
+                } else {
+                    const pass = data.password;
+                    const name = data.username;
+                    bcrypt.compare(password,pass,function(err,pass){
+                        if(err){
+                            console.log(err); //
+                        } else {
+                            if(pass){
+                                res.render('dashboard',{name:name});
+                            } else{
+                                res.render("login",{title:"Login",error:"",msg:"Please check your password !"})
+                            }
+                        }
+                    });
+                }
+            });
+        
+        }
+    });
+    
 }
 
